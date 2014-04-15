110c8fe4d5db67f4b706993acb7a62c34a4f0c8c
StefanRvo <stefan@stefanrvo.dk>
2014-04-15 11:35:45 +0200

    changed to python3 added repeat field

--- terminal.py
+++ terminal.py
@@ -1,10 +1,10 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import time
 from serial import Serial
 import sys
 from math import log
-import Tkinter
+import tkinter
 connected=0
 serialport=0
 
@@ -53,49 +53,58 @@ def SendInput():
         command=InputString.get()
         command=int(command,16)
         command=ToHexList(command)
-        serialport.write(command)
+        for i in range(int(RepeatString.get())):
+            serialport.write(command)
+            time.sleep(0.001)
     
 def Recive():
     reading=[]
     if connected:
         global RecievedString
         while (serialport.inWaiting()>0):							        
-            if (serialport.inWaiting > 0):			
+            if (serialport.inWaiting() > 0):			
                 reading.append(ord(serialport.read(1)))
             time.sleep(0.001)
         if reading!=[]:
             hexstring=''
             for i in reading:
                 hexstring+=hex(i)[2:]+' '
-            RecievedField.insert(Tkinter.INSERT,RecievedString.get()+hexstring+'\n')
+            RecievedField.insert(tkinter.INSERT,RecievedString.get()+hexstring+'\n')
+            RecievedField.see('end')
     top.after(1,Recive)
 
 
 
 
 
-top=Tkinter.Tk() #Main Windows
+top=tkinter.Tk() #Main Windows
 RWidth=top.winfo_screenwidth()
 RHeight=top.winfo_screenheight()
 top.minsize(50, 50)
 top.maxsize(RWidth, RHeight)
-InputString=Tkinter.StringVar() #String for the input window
-RecievedString=Tkinter.StringVar()
-PortPath=Tkinter.StringVar()
-SendButton=Tkinter.Button(top,command=SendInput,text="Send",anchor='w') #Button to send
-PortField=Tkinter.Entry(top,textvariable=PortPath)
+InputString=tkinter.StringVar() #String for the input window
+RecievedString=tkinter.StringVar()
+RepeatString=tkinter.StringVar()
+RepeatString.set('1')
+PortPath=tkinter.StringVar()
+SendButton=tkinter.Button(top,command=SendInput,text="Send",anchor='w') #Button to send
+PortField=tkinter.Entry(top,textvariable=PortPath)
 PortField.grid(row=90, column=0, sticky="nsew")
-ConnectButton=Tkinter.Button(top,command=SerialConnect,text="Connect")
-ConnectButton.grid(row=90, column=1, sticky="nsew")
-InputLabel=Tkinter.Label(top,text="Input:")
+ConnectButton=tkinter.Button(top,command=SerialConnect,text="Connect")
+ConnectButton.grid(row=90, column=1, columnspan=2, sticky="nsew")
+InputLabel=tkinter.Label(top,text="Input:")
 InputLabel.grid(row=100, column=0, sticky="nsew")
+RepeatLabel=tkinter.Label(top,text="Repeats:")
 
-InputField=Tkinter.Entry(top,textvariable=InputString) #Field for user input
+InputField=tkinter.Entry(top,textvariable=InputString) #Field for user input
+RepeatField=tkinter.Entry(top,textvariable=RepeatString) #Field for user input
+RepeatLabel.grid(row=100,column=1)
+RepeatField.grid(row=100,column=2)
 InputField.grid(row=110, column=0, sticky="nsew") #Show input Field
-SendButton.grid(row=110, column=1, sticky="nsew") #Draw the button
-RecievedScrool=Tkinter.Scrollbar(top)
+SendButton.grid(row=110, column=1, columnspan=2, sticky="nsew") #Draw the button
+RecievedScrool=tkinter.Scrollbar(top)
 RecievedScrool.grid(row=1200,column=1000,sticky="nsew")
-RecievedField=Tkinter.Text(top,bg='white',yscrollcommand=RecievedScrool.set)
+RecievedField=tkinter.Text(top,bg='white',yscrollcommand=RecievedScrool.set)
 RecievedField.grid(row=1200,columnspan=999,  sticky="nsew")
 
 #RecievedField.pack()
